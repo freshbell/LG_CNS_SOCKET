6fa102fd7dc27f0046b5eea2e6b7ac9b268aa587
freshbell <freshbell95@gmail.com>
2021-11-25 01:55:55 +0900

    Initial commit

---
+++ client.py
@@ -0,0 +1,86 @@
+import socket 
+import threading 
+import json
+import time
+from collections import deque
+import random
+
+# JSON 파일 open
+with open('./JSON/client/Report.json', 'r', encoding='UTF-8') as f:
+    STATE_JSON = json.load(f)
+with open('./JSON/client/Alarm.json', 'r', encoding='UTF-8') as f:
+    ALARM_JSON = json.load(f)
+
+# 알람 전송
+ALARM_CD_LIST = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+ALARM_CD_USED = deque([])
+temp_end_alarm = 10
+temp_start_alarm = 10
+ALARM_REPORT_JSON = {
+    'DATA_TYPE': 'alarm',
+    'AGV_NO': 'TEMP',
+    'ALARMS': []
+}
+
+# 랜덤 ALARM_CD
+def random_alarm():
+    global temp_end_alarm, temp_start_alarm
+
+    temp_start_alarm = random.choice(ALARM_CD_LIST)
+    ALARM_CD_LIST.remove(temp_start_alarm)
+    ALARM_CD_USED.append(temp_start_alarm)
+    
+    # 알람이 발생하고 5초 후 해제
+    if(len(ALARM_CD_USED) == 6):
+        temp_end_alarm = ALARM_CD_USED.popleft()
+        ALARM_CD_LIST.append(temp_end_alarm)
+
+    ALARM_JSON['ALARMS'][temp_start_alarm]['ALARM_STATUS'] = 1
+    ALARM_JSON['ALARMS'][temp_start_alarm]['OCCUR_DT'] = time.strftime('20%y%m%d %H:%M:%S')
+    ALARM_JSON['ALARMS'][temp_start_alarm]['END_DT'] = None
+
+    ALARM_REPORT_JSON['ALARMS'] = []
+    if(temp_end_alarm != 10):
+        ALARM_REPORT_JSON['ALARMS'].append(ALARM_JSON['ALARMS'][temp_end_alarm])
+        ALARM_JSON['ALARMS'][temp_end_alarm]['END_DT'] = time.strftime('20%y%m%d %H:%M:%S')
+        ALARM_JSON['ALARMS'][temp_end_alarm]['ALARM_STATUS'] = 0
+
+    ALARM_REPORT_JSON['ALARMS'].append(ALARM_JSON['ALARMS'][temp_start_alarm])
+
+    return ALARM_REPORT_JSON
+
+def Send(client_sock): 
+    client_sock.send(AGV_NO.encode())
+    while True: 
+        time.sleep(1)
+        send_data = json.dumps(random_alarm(),ensure_ascii=False).encode()
+        client_sock.send(send_data)
+
+def Recv(client_sock): 
+    while True: 
+        recv_data = client_sock.recv(2048).decode() 
+        recv_data = json.loads(recv_data)
+        data_type = recv_data['DATA_TYPE']
+        if data_type == 'moveCommand':
+            pass
+        elif data_type == 'reportRqst':
+            client_sock.send(json.dumps(STATE_JSON,ensure_ascii=False).encode())
+
+#TCP Client 
+if __name__ == '__main__': 
+    AGV_NO = 'AGV00005'
+
+    STATE_JSON['AGV_NO'] = AGV_NO
+    ALARM_REPORT_JSON['AGV_NO'] = AGV_NO
+
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    Host = 'localhost' 
+    Port = 5000
+    client_sock.connect((Host, Port))
+    print('Connecting to ', Host, Port)
+    
+    send_thread = threading.Thread(target=Send, args=(client_sock, )) 
+    send_thread.start()
+    
+    recv_thread = threading.Thread(target=Recv, args=(client_sock, )) 
+    recv_thread.start()
---
+++ server.py
@@ -0,0 +1,134 @@
+import socket 
+import threading 
+from queue import Queue 
+import json
+import time
+import random
+
+# JSON 파일 open
+with open('./JSON/server/Request.json', 'r', encoding='UTF-8') as f:
+    STATE_REQUEST = json.load(f)
+with open('./JSON/server/Move.json', 'r', encoding='UTF-8') as f:
+    MOVE_JSON = json.load(f)
+
+# 로그 저장 text
+now = time.strftime('20%y%m%d %H%M%S')
+alarm_f = open("./log/alarm/alarm" + now + ".txt","w", encoding='utf-8')
+state_f = open("./log/state/state" + now + ".txt","w", encoding='utf-8')
+
+# thread 종료
+chk = True
+
+# 클라이언트
+clients = {}
+
+def make_route():
+    # 맵 크기
+    MAX_N = MAX_M = 30 
+    direction_x = [1,0,-1,0]
+    direction_y = [0,1,0,-1]
+
+    x = random.sample(range(1, 31),1)[0]
+    y = random.sample(range(1, 31),1)[0]
+    BLOCKS = [str(x).zfill(4) + str(y).zfill(4)]
+
+    x, y = random.sample(range(1,31),1)[0], random.sample(range(1,31),1)[0]
+    for _ in range(random.sample(range(20, 30),1)[0]):
+        while True:
+            direction = random.sample(range(0,3),1)[0]
+            if 0 < x + direction_x[direction] <= MAX_N and 0 < y + direction_y[direction] <= MAX_M:
+                x, y = x + direction_x[direction], y + direction_y[direction]
+                break
+        BLOCKS.append(str(x).zfill(4) + str(y).zfill(4))
+
+    return BLOCKS
+
+def Send(group, send_queue):
+    global clients
+
+    print('Thread Send Start') 
+    while chk:
+        try: #새롭게 추가된 클라이언트가 있을 경우 Send 쓰레드를 새롭게 만들기 위해 루프를 빠져나감 
+            recv = ''
+            if not send_queue.empty():
+                recv = send_queue.get()
+            
+            if recv == 'Group Changed': 
+                break
+
+            time.sleep(0.05)
+            for conn in group:
+                STATE_REQUEST['AGV_NO'] = clients[conn]['AGV_NO']
+                MOVE_JSON['AGV_NO'] = clients[conn]['AGV_NO']
+                MOVE_JSON['BLOCKS'] = clients[conn]['BLOCKS']
+
+                state = json.dumps(STATE_REQUEST,ensure_ascii=False).encode()
+                move = json.dumps(MOVE_JSON, ensure_ascii=False).encode()
+
+                conn.send(state)
+                time.sleep(0.000001)
+                conn.send(move)
+        except: 
+            pass 
+
+def Recv(conn, count):
+    global clients
+
+    print('Thread Recv' + str(count) + ' Start') 
+
+    AGV_NO = conn.recv(2048).decode()
+    clients[conn] = {}
+    clients[conn]['AGV_NO'] = AGV_NO
+    clients[conn]['BLOCKS'] = make_route()
+
+    while chk:
+        data = conn.recv(2048).decode()
+        data = json.loads(data)
+        
+        data_type = data['DATA_TYPE']
+
+        if data_type == 'alarm':
+            alarm_f.write(str(data) + '\n')
+        elif data_type == 'report':
+            data['TIME'] = time.strftime('20%y%m%d %H%M%S')
+            state_f.write(str(data) + '\n')
+
+def input_a():
+    global chk
+    while True:
+        a = int(input())
+        if a == 0:
+            chk = False
+            break
+
+if __name__ == '__main__': 
+    send_queue = Queue() 
+    
+    HOST = '' # 수신 받을 모든 IP를 의미 
+    PORT = 5000 # 수신받을 Port 
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP Socket 
+    server_sock.bind((HOST, PORT)) # 소켓에 수신받을 IP주소와 PORT를 설정 
+    server_sock.listen(10) # 소켓 연결, 여기서 파라미터는 접속수를 의미 
+    count = 0
+
+    thread3 = threading.Thread(target=input_a)
+    thread3.start()
+
+    group = [] #연결된 클라이언트의 소켓정보를 리스트로 묶기 위함 
+    while chk:
+        count = count + 1 
+        conn, addr = server_sock.accept() # 해당 소켓을 열고 대기 
+        group.append(conn) #연결된 클라이언트의 소켓정보 
+        print('Connected ' + str(addr)) #소켓에 연결된 모든 클라이언트에게 동일한 메시지를 보내기 위한 쓰레드(브로드캐스트) #연결된 클라이언트가 1명 이상일 경우 변경된 group 리스트로 반영 
+        
+        if count > 1: 
+            send_queue.put('Group Changed')
+            thread1 = threading.Thread(target=Send, args=(group, send_queue,)) 
+            thread1.start() 
+            pass 
+        else: 
+            thread1 = threading.Thread(target=Send, args=(group, send_queue,)) 
+            thread1.start() #소켓에 연결된 각각의 클라이언트의 메시지를 받을 쓰레드 
+
+        thread2 = threading.Thread(target=Recv, args=(conn, count,)) 
+        thread2.start()
