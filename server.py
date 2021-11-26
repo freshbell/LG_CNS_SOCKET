cc77061c60dd6c8b57797c6e5a92758a2f6818e6
freshbell <freshbell95@gmail.com>
2021-11-27 04:12:40 +0900

    commit

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import json
 import time
 import random
 import sys
+import logging
 
 # JSON 파일 open
 with open('./JSON/server/Request.json', 'r', encoding='UTF-8') as f:
@@ -17,6 +18,8 @@ with open('./JSON/server/Move.json', 'r', encoding='UTF-8') as f:
 now = time.strftime('20%y%m%d %H%M%S')
 alarm_f = open("./log/alarm/alarm" + now + ".txt","w", encoding='utf-8')
 state_f = open("./log/state/state" + now + ".txt","w", encoding='utf-8')
+
+logging.basicConfig(filename='./log/debug.log', encoding='utf-8', level=logging.DEBUG)
 
 # thread 종료
 chk = True
@@ -66,7 +69,7 @@ def Send(group, send_queue):
                 conn.send((state+move).encode())
 
         except: 
-            print('fail')
+            #print()
             pass 
 
 def Recv(conn, count):
@@ -78,20 +81,23 @@ def Recv(conn, count):
     clients[conn]['BLOCKS'] = make_route()
     
     while chk:
-        data = json.loads(conn.recv(2048).decode())
+        data = conn.recv(2048).decode()
+        data = json.loads(data)
         
         data_type = data['DATA_TYPE']
         if data_type == 'alarm':
+            logging.info('alarm json -' + str(data))
             alarm_f.write(str(data) + '\n')
         elif data_type == 'report':
+            logging.info('state json -' + str(data))
             data['TIME'] = time.strftime('20%y%m%d %H%M%S')
             state_f.write(str(data) + '\n')
 
-def input_a():
+def input_exit_chk():
     global chk
     while True:
-        a = int(input())
-        if a == 0:
+        exit_chk = int(input())
+        if exit_chk == 0:
             chk = False
             break
 
@@ -102,12 +108,20 @@ if __name__ == '__main__':
     send_queue = Queue() 
     HOST = '' # 수신 받을 모든 IP를 의미 
     PORT = 5001 # 수신받을 Port 
+    
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP Socket 
+    logging.debug('Socket Created')
+
     server_sock.bind((HOST, PORT)) # 소켓에 수신받을 IP주소와 PORT를 설정 
+    logging.debug('Socket Bind Complete')
+
     server_sock.listen(10) # 소켓 연결, 여기서 파라미터는 접속수를 의미 
+    logging.debug('Socket Listening')
+
     count = 0
 
-    thread3 = threading.Thread(target=input_a)
+    # 종료 감지 스레드
+    thread3 = threading.Thread(target=input_exit_chk)
     thread3.start()
     
     group = [] #연결된 클라이언트의 소켓정보를 리스트로 묶기 위함 
@@ -119,8 +133,8 @@ if __name__ == '__main__':
             conn, addr = server_sock.accept() # 해당 소켓을 열고 대기 
             group.append(conn) #연결된 클라이언트의 소켓정보 
             count = count + 1
-            print('Connected ' + str(addr)) #소켓에 연결된 모든 클라이언트에게 동일한 메시지를 보내기 위한 쓰레드(브로드캐스트) #연결된 클라이언트가 1명 이상일 경우 변경된 group 리스트로 반영 
-            
+            print('Connected ' + str(addr[0]) + ':' + str(addr[1])) #소켓에 연결된 모든 클라이언트에게 동일한 메시지를 보내기 위한 쓰레드(브로드캐스트) #연결된 클라이언트가 1명 이상일 경우 변경된 group 리스트로 반영 
+            logging.debug('Connected ' + str(addr[0]) + ':' + str(addr[1]))
             if count > 1: 
                 send_queue.put('Group Changed')
                 thread1 = threading.Thread(target=Send, args=(group, send_queue,)) 
